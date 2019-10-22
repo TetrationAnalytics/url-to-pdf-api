c33baa59fedb75aca75d319bca474540302576f0
Tony Lee <tlee3@tetrationanalytics.com>
2019-10-22 14:55:41 -0700

    use namespace env variables

--- src/config.js
+++ src/config.js
@@ -2,31 +2,31 @@
 
 // Env vars should be casted to correct types
 const config = {
-  PORT: Number(process.env.PORT) || 9000,
+  PORT: Number(process.env.PDF_PORT) || 9000,
   NODE_ENV: process.env.NODE_ENV,
-  LOG_LEVEL: process.env.LOG_LEVEL,
-  ALLOW_HTTP: true, //process.env.ALLOW_HTTP === 'true',
-  DEBUG_MODE: true, //process.env.DEBUG_MODE === 'true',
-  IGNORE_HTTPS_ERRORS: true, //process.env.IGNORE_HTTPS_ERRORS === 'true',
-  CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
+  LOG_LEVEL: process.env.PDF_LOG_LEVEL,
+  ALLOW_HTTP: process.env.PDF_ALLOW_HTTP === 'true',
+  DEBUG_MODE: process.env.PDF_DEBUG_MODE === 'true',
+  IGNORE_HTTPS_ERRORS: process.env.PDF_IGNORE_HTTPS_ERRORS === 'true',
+  CORS_ORIGIN: process.env.PDF_CORS_ORIGIN || '*',
   API_TOKENS: [],
-  BASEHOST: process.env.BASEHOST,
-  PROTOCOL: process.env.PROTOCOL,
-  USERNAME_SELECTOR: process.env.USERNAME_SELECTOR,
-  PASSWD_SELECTOR: process.env.PASSWD_SELECTOR,
-  SIGN_IN_SELECTOR: process.env.SIGN_IN_SELECTOR,
-  AUTH_PATH: process.env.AUTH_PATH,
-  AUTH_USERNAME: process.env.AUTH_USERNAME,
-  AUTH_PASSWORD: process.env.AUTH_PASSWORD,
-  ALLOW_UNKNOWN_BODY: process.env.ALLOW_UNKNOWN_BODY,
-  HDFS_USER: process.env.HDFS_USER,
-  HDFS_HOST: process.env.HDFS_HOST,
-  HDFS_PORT: process.env.HDFS_PORT,
-  HDFS_PATH: process.env.HDFS_PATH,
+  BASEHOST: process.env.PDF_BASEHOST,
+  PROTOCOL: process.env.PDF_PROTOCOL,
+  USERNAME_SELECTOR: process.env.PDF_USERNAME_SELECTOR,
+  PASSWD_SELECTOR: process.env.PDF_PASSWD_SELECTOR,
+  SIGN_IN_SELECTOR: process.env.PDF_SIGN_IN_SELECTOR,
+  AUTH_PATH: process.env.PDF_AUTH_PATH,
+  AUTH_USERNAME: process.env.PDF_AUTH_USERNAME,
+  AUTH_PASSWORD: process.env.PDF_AUTH_PASSWORD,
+  ALLOW_UNKNOWN_BODY: process.env.PDF_ALLOW_UNKNOWN_BODY,
+  HDFS_USER: process.env.PDF_HDFS_USER,
+  HDFS_HOST: process.env.PDF_HDFS_HOST,
+  HDFS_PORT: process.env.PDF_HDFS_PORT,
+  HDFS_PATH: process.env.PDF_HDFS_PATH,
 };
 
-if (process.env.API_TOKENS) {
-  config.API_TOKENS = process.env.API_TOKENS.split(',');
+if (process.env.PDF_API_TOKENS) {
+  config.API_TOKENS = process.env.PDF_API_TOKENS.split(',');
 }
 
 module.exports = config;
