1f1663d8a31ecbed85f582cf9ebb57982602d651
Tony Lee <tlee3@tetrationanalytics.com>
2019-10-22 14:55:24 -0700

    use dotenv to load .env

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+const path = require('path');
+const dotenv = require('dotenv');
+
+const envPath = path.resolve(__dirname, '../.env');
+dotenv.config({path: envPath});
 
 const createApp = require('./app');
 const enableDestroy = require('server-destroy');
